338926eb510a909e0e7da3668553a90941322fd0
Oliver Nygren <oliver.nygren@icloud.com>
2021-02-11 15:21:04 +0100

    app

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      url: 'https://olivernygrenweb.se/',
+      showBoots: true,
+      boots: [
+        { title: 'Green Boots', brand: 'Vagabond', img: './assets/green.jpg', isFav: true, price: 799 },
+        { title: 'Brown Boots', brand: 'Adidas', img: './assets/brown.jpg', isFav: false, price: 999 },
+        { title: 'Red Boots', brand: 'Louis Vuitton', img: './assets/red.jpg', isFav: true, price: 1299 }
+      ]
+    }
+  },
+  methods: {
+    toggleShowBoots() {
+      this.showBoots = !this.showBoots
+    },
+    toggleFavs(boot) {
+      boot.isFav = !boot.isFav
+    }
+  },
+  // computed: {
+  //   filteredBoots() {
+  //     return this.boots.filter((boot) => boot.isFav)
+  //   }
+  // }
+})
+
+app.mount('#app')
